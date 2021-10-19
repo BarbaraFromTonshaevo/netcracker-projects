77dab60da8a9b04a48509974a51dec47307a3c57
Elkina Barbara <elkinabarbara1999@gmail.com>
2021-10-19 14:37:54 +0300

    with not-found

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -32,10 +32,10 @@ export const routes: Routes = [
     path: 'not-found',
     component: NotFoundComponent,
   },
-  // {
-  //   path: '**',
-  //   redirectTo: 'not-found'
-  // }
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  }
 ];
 
 @NgModule({
